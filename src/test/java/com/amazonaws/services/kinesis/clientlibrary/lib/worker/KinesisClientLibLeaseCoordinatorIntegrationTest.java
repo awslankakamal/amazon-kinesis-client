d240be9075f0ef37d3beb4bf255833ee8bf6f70c
Lanka <vlanka@amazon.com>
2019-03-17 21:48:37 -0700

    Update KinesisClientLibLeaseCoordinatorIntegrationTest.java

--- src/test/java/com/amazonaws/services/kinesis/clientlibrary/lib/worker/KinesisClientLibLeaseCoordinatorIntegrationTest.java
+++ src/test/java/com/amazonaws/services/kinesis/clientlibrary/lib/worker/KinesisClientLibLeaseCoordinatorIntegrationTest.java
@@ -11,7 +11,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */age com.amazonaws.services.kinesis.clientlibrary.lib.worker;
+ */
+package com.amazonaws.services.kinesis.clientlibrary.lib.worker;
 
 import java.util.ArrayList;
 import java.util.Collections;
